73ba2dd6d9d4a1fbba20914c59e34cd26d9fa0ce
mathieu.cailly <mathieu.cailly@gmail.com>
2022-03-12 03:54:48 +0100

    wip

--- middleware/is_admin.js
+++ middleware/is_admin.js
@@ -1,7 +1,8 @@
-export default function ({ $auth, redirect }) {
+export default function ({ $auth, redirect, next }) {
     // console.log($auth.hasScope('ADMIN'))
     // console.log($auth.user.role)
     if ($auth.user.role != 'ADMIN') {
         return redirect('/')
     }
+    next()
 }
